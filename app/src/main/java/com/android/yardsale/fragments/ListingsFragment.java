7ce7bf153a9ca6cb39ed914e9982126b66d702f3
pshegde <pshegde@ncsu.edu>
2015-06-23 00:08:27 -0700

    floating buttons

--- app/src/main/java/com/android/yardsale/fragments/ListingsFragment.java
+++ app/src/main/java/com/android/yardsale/fragments/ListingsFragment.java
@@ -89,7 +89,7 @@ public class ListingsFragment extends Fragment {
         registerForContextMenu(rvSales);
 
         btCreateSale = (FloatingActionButton) view.findViewById(R.id.fab);
-        btCreateSale.setImageDrawable((getResources().getDrawable(R.drawable.ic_action_content_add)));
+        //btCreateSale.setImageDrawable((getResources().getDrawable(R.drawable.ic_action_content_add)));
 //        btFlip.setColorNormal(R.color.amber);
 //        btFlip.setColorPressed(R.color.amber);
 //
