9eb402199d4daf6700fb5141b27fe21f973d546e
Ashwini Thonteswaramurthy <ashwini.thonteswaramurthy@gmail.com>
2015-06-23 13:19:28 -0700

    styling the alert dialogs

--- app/src/main/java/com/android/yardsale/fragments/YouDoNotOwnThisAlertDialog.java
+++ app/src/main/java/com/android/yardsale/fragments/YouDoNotOwnThisAlertDialog.java
@@ -4,6 +4,10 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.android.yardsale.R;
 
@@ -22,11 +26,14 @@ public class YouDoNotOwnThisAlertDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View alertDialogLayout = inflater.inflate(R.layout.alert_dialog_layout, null);
         String tag = getArguments().getString("tag");
+        Log.e("ON_ALERT", tag + alertDialogLayout.getTag());
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-        alertDialogBuilder.setTitle("Error!!");
-        alertDialogBuilder.setIcon(R.drawable.ic_warning);
-        alertDialogBuilder.setMessage(tag);
+        alertDialogBuilder.setView(alertDialogLayout);
+        TextView tvMessage = (TextView) alertDialogLayout.findViewById(R.id.tvMessage);
+        tvMessage.setText(tag);
         return alertDialogBuilder.create();
     }
 
